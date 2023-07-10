7f98c86d2a35e2c9d12db3038c0f3223854a6d84
codemazui <codemazui@gmail.com>
2023-07-09 22:03:45 -0300

    update

---
+++ 12_Net/ex_12_2.py
@@ -0,0 +1,10 @@
+import urllib.request, urllib.parse, urllib.error
+
+fh = urllib.request.urlopen("http://data.pr4e.org/romeo.txt")
+
+count = dict()
+for line in fh:
+    words = line.decode().split()
+    for word in words:
+        count[word] = count.get(word, 0) + 1
+print(count)
